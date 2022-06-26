6c771f76af96964ef87b4eb38c70656a88dcebcf
hema_ <hema_eie552@yahoo.com>
2022-06-26 21:45:15 +0200

    last edit

--- lib/ui_screens/auth/login.dart
+++ lib/ui_screens/auth/login.dart
@@ -106,7 +106,7 @@ class _MobileState extends State<Mobile> {
           SizedBox(
             height: Responsive.height(3.0, context),
           ),
-          widget.type == 'doctor'
+          widget.type != 'Patient'
               ? SizedBox()
               : GestureDetector(
                   onTap: () {
